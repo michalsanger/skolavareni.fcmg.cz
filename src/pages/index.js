9ef283101ecacb1de99ddd6681dc52170f94f3b5
Michal Sanger <michal.sanger@gmail.com>
2021-04-18 19:10:13 +0200

    Homepage photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,62 +5,79 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
+import Image from "@theme/IdealImage";
 
 const features = [];
 
 function Feature({ imageUrl, title, description }) {
-	const imgUrl = useBaseUrl(imageUrl);
-	return (
-		<div className={clsx("col col--4", styles.feature)}>
-			{imgUrl && (
-				<div className="text--center">
-					<img className={styles.featureImage} src={imgUrl} alt={title} />
-				</div>
-			)}
-			<h3>{title}</h3>
-			<p>{description}</p>
-		</div>
-	);
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx("col col--4", styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 export default function Home() {
-	const context = useDocusaurusContext();
-	const { siteConfig = {} } = context;
-	return (
-		<Layout
-			title={`Hello from ${siteConfig.title}`}
-			description="Description will go into a meta tag in <head />"
-		>
-			<header className={clsx("hero hero--primary", styles.heroBanner)}>
-				<div className="container">
-					<h1 className="hero__title">{siteConfig.title}</h1>
-					<p className="hero__subtitle">{siteConfig.tagline}</p>
-					<div className={styles.buttons}>
-						<Link
-							className={clsx(
-								"button button--secondary button--lg",
-								styles.getStarted
-							)}
-							to={useBaseUrl("recepty/")}
-						>
-							Zobrazit recepty
-						</Link>
-					</div>
-				</div>
-			</header>
-			<main>
-				{features && features.length > 0 && (
-					<section className={styles.features}>
-						<div className="container">
-							<div className="row">
-								{features.map((props, idx) => (
-									<Feature key={idx} {...props} />
-								))}
-							</div>
-						</div>
-					</section>
-				)}
-			</main>
-		</Layout>
-	);
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                "button button--secondary button--lg",
+                styles.getStarted
+              )}
+              to={useBaseUrl("recepty/")}
+            >
+              Zobrazit recepty
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section style={{ marginTop: "2em" }}>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <Image img={require("./hp01.jpg")} />
+              </div>
+              <div className="col">
+                <Image img={require("./hp02.jpg")} />
+              </div>
+              <div className="col">
+                <Image img={require("./hp03.jpg")} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
 }
